fc6011483cbadce8f97120f9bd4640562c9a6d0c
Luis Ruiz <lruizr23_1@unc.edu.pe>
2026-02-08 21:41:45 -0500

    Creacion de los fragments

--- app/src/main/java/sistemas/unc/edu/appadopcionmascotas/ActividadRefugio.java
+++ app/src/main/java/sistemas/unc/edu/appadopcionmascotas/ActividadRefugio.java
@@ -11,6 +11,10 @@ import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import sistemas.unc.edu.appadopcionmascotas.fragments.DashboardFragment;
+import sistemas.unc.edu.appadopcionmascotas.fragments.MensajesFragment;
+import sistemas.unc.edu.appadopcionmascotas.fragments.PerfilFragment;
+
 public class ActividadRefugio extends AppCompatActivity {
 
     BottomNavigationView bottom_nav_refugio;
@@ -21,7 +25,32 @@ public class ActividadRefugio extends AppCompatActivity {
 
         bottom_nav_refugio = findViewById(R.id.bottom_nav_refugio);
 
+        cargarFragment(new DashboardFragment());
+        bottom_nav_refugio.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
 
+            if(id == R.id.itemDashboard){
+                selectedFragment = new DashboardFragment();
+            }
+            else if(id == R.id.itemMensajes){
+                selectedFragment = new MensajesFragment();
+            }
+            else if(id == R.id.itemPerfil){
+                selectedFragment = new PerfilFragment();
+            }
+            return cargarFragment(selectedFragment);
+        });
     }
 
+    private boolean cargarFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out) //una animacion
+                    .replace(R.id.contenedor_refugio, fragment)
+                    .commit();
+        }
+        return true;
+    }
 }
